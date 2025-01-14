be4286cb1ed4f17919f9a2c93310fcd5b0aa3b71
kristevii <2024tevi@gmail.com>
2025-01-14 12:37:11 +0700

    halaman

---
+++ app/src/main/java/com/kristevi/alsbitessolo/TentangActivity.kt
@@ -0,0 +1,20 @@
+package com.kristevi.alsbitessolo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TentangActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tentang)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
